9333e8ed2fd5a9d74e210412cdc7f62952ae046f
Miika Pietila <mii@iki.fi>
2014-02-20 02:13:56 +0200

    first on printerping

--- bot/pajabot.py
+++ bot/pajabot.py
@@ -181,11 +181,11 @@ class PajaBot(SingleServerIRCBot):
                 if (cmd=='!checksum') or (cmd=='!checksum'):
                         self.say('pixelvar: ' + str(self.camera.checkSum()))
                 if (cmd=='!printer') or (cmd=='!tulostin'):
-                        ping_response = subprocess.Popen(["/bin/ping", "-c1", "-w100", "10.10.99.3"], stdout=subprocess.PIPE).stdout.read()
-                        if ('unreachable' in ping_response):
-                          self.say('printer is offline')
-                        else:
+                        ping_response = subprocess.Popen(["/bin/ping", "-c1", "-w2", "10.10.99.3"], stdout=subprocess.PIPE).stdout.read()
+                        if ('rtt' in ping_response):
                           self.say('printer is online')
+                        else:
+                          self.say('printer is offline')
                         print('p: ' + str(ping_response))
 #                        self.say('p: ' + str(ping_response))
 
