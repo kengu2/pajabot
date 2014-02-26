aced526be3aec97863ad44ef965866be4b42145f
Miika Pietila <mii@iki.fi>
2014-02-26 16:33:55 +0200

    tabs to spaces

--- bot/gpio_pins.py
+++ bot/gpio_pins.py
@@ -3,18 +3,18 @@ import RPi.GPIO as GPIO
 # RPi GPIO pin stuff (NOT USED ATM)
 
 class GpioPins():
-        def __init__(self):
-                self.doorpin = 11
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(self.doorpin, GPIO.IN)
-                self.doorStatus = GPIO.input(self.doorpin)
+    def __init__(self):
+        self.doorpin = 11
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.doorpin, GPIO.IN)
+        self.doorStatus = GPIO.input(self.doorpin)
 
-	def pollPins(self)
-                newDs = GPIO.input(self.doorpin)
-                if newDs is not self.doorStatus:
-                    self.doorStatus = newDs
-                    self.sayDoorStatus()
+    def pollPins(self)
+        newDs = GPIO.input(self.doorpin)
+        if newDs is not self.doorStatus:
+            self.doorStatus = newDs
+            self.sayDoorStatus()
 
-	def close(self):
-		GPIO.cleanup()
+    def close(self):
+        GPIO.cleanup()
 
