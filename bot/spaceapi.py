9ecf1cfba9ca5f9b81612d351f94a0062841b565
Ville Ranki <ville.ranki@nomovok.com>
2014-09-01 18:34:23 +0300

    Fix

--- bot/spaceapi.py
+++ bot/spaceapi.py
@@ -1,9 +1,5 @@
 import zerorpc
 
-c = zerorpc.Client()
-c.connect("tcp://127.0.0.1:4242")
-print c.updateStatus(True, "testing")
-
 class SpaceAPI(object):
     def __init__(self, url):
 	print "SpaceAPI connecting to " + url
