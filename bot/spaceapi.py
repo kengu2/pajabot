90e6dfa3d584c17f66befd1d1c0eaec653e5a9b0
Ville Ranki <ville.ranki@nomovok.com>
2014-09-01 18:35:23 +0300

    Fix2

--- bot/spaceapi.py
+++ bot/spaceapi.py
@@ -3,11 +3,11 @@ import zerorpc
 class SpaceAPI(object):
     def __init__(self, url):
 	print "SpaceAPI connecting to " + url
-	c = zerorpc.Client()
-	c.connect(url)
+	self.c = zerorpc.Client()
+	self.c.connect(url)
 
     def updateStatus(self, labOpen, topic):
 	print "Setting lab open " + str(labOpen) + " and topic " + topic
-	c.updateStatus(labOpen, topic)
+	self.c.updateStatus(labOpen, topic)
         print "Update ok"
 
