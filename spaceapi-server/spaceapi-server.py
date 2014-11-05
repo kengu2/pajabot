439c8d0030af0b698fb153d46bfcf84543ee5989
Miika Pietila <mii@iki.fi>
2014-11-05 16:15:18 +0200

    First proper spaceapi-server implementation on zerorpc

--- spaceapi-server/spaceapi-server.py
+++ spaceapi-server/spaceapi-server.py
@@ -1,11 +1,22 @@
 import zerorpc
+import re
+import time
+
+with open('spaceapi.json.template','r') as f:
+    templatefile = f.read();
+
+print templatefile
 
 class SpaceAPI(object):
     def updateStatus(self, labOpen, topic):
-	print "Set lab open " + str(labOpen) + " and topic " + topic
+        print "Set lab open " + str(labOpen) + " and topic " + topic
+        updatedfile2 = re.sub("STATUS_OPEN", str(labOpen) , templatefile)
+        updatedfile = re.sub("STATUS_LASTCHANGE", str(int(time.time())), updatedfile2)
+        outfile = open('spaceapi.json', 'w')
+        outfile.write(updatedfile)
+        outfile.close
         return "Update ok"
 
 s = zerorpc.Server(SpaceAPI())
 s.bind("tcp://0.0.0.0:4242")
 s.run()
-
