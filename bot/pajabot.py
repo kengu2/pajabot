03b5c215b3011beaffcf5def4a84507082bff354
kengu2 <mii@iki.fi>
2016-01-16 17:51:44 +0200

    Update pajabot.py
    temporary hack, iioo to camera

--- bot/pajabot.py
+++ bot/pajabot.py
@@ -111,11 +111,12 @@ class PajaBot(SingleServerIRCBot):
         self.doorStatus = None
         self.spaceapi = SpaceAPI(config.get("bot", "spaceapiurl"))
         self.camera = RPiCamera()
-        if (self.vaasa):
-            from iioo import IiOo
-            self.iioo = IiOo()
-        else:
-            self.iioo = RpiCamera()
+#        if (self.vaasa):
+#            from iioo import IiOo
+#            self.iioo = IiOo()
+#        else:
+#            self.iioo = RpiCamera()
+        self.iioo = RpiCamera()
         self.lightStatus = self.iioo.checkLights()
         self.statusMessage = "Hello world"
 
